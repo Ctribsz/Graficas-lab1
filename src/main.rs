0227d05be86d0090f6d632cba3d399e2e8f0539f
Ctribsz <122636369+Ctribsz@users.noreply.github.com>
2024-08-07 02:37:07 -0600

    lab1 - Poligon2

--- src/main.rs
+++ src/main.rs
@@ -11,9 +11,15 @@ use bmp::BMP;
 fn main() {
     let mut fb = FrameBuffer::new(800, 600);
 
+    //Poligono 1
     let points1 = [
         (165, 380), (185, 360), (180, 330), (207, 345), (233, 330),
         (230, 360), (250, 380), (220, 385), (205, 410), (193, 383),
+    ];
+
+    //Poligono 2
+    let points2 = [
+        (321, 335), (288, 286), (339, 251), (374, 302),
     ];
 
     // Rellenar el primer polígono con color amarillo
@@ -26,6 +32,16 @@ fn main() {
         draw_line(&mut fb, x0, y0, x1, y1, Color::WHITE);
     }
 
+    // Rellenar el segundo polígono con color azul
+    fill_polygon(&mut fb, &points2, Color::BLUE);
+
+    // Dibujar la orilla del segundo polígono con color blanco
+    for i in 0..points2.len() {
+        let (x0, y0) = points2[i];
+        let (x1, y1) = points2[(i + 1) % points2.len()];
+        draw_line(&mut fb, x0, y0, x1, y1, Color::WHITE);
+    }
+
     // Guardar el framebuffer como BMP
-    BMP::save_as_bmp(&fb, "polygon_1.bmp").unwrap();
+    BMP::save_as_bmp(&fb, "polygons.bmp").unwrap();
 }
